fb64e89302ce1d97e51d4c9580c6fd97fb83ab5d
Mohammed Sallout <93588232+MzSalout@users.noreply.github.com>
2022-05-17 20:03:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let group = document.createElement("div");
 group.setAttribute("class", "group");
 container.appendChild(group);
 
-fetch("https://inshorts.deta.dev/news?category=technology")
+fetch("http://inshorts.deta.dev/news?category=technology")
   .then((res) => {
     return res.json();
   })
